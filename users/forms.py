68d2d00f2ae12183da8860e8d42330038076a5ea
alexdeitos <alexdeitos01@gmail.com>
2022-02-22 16:21:49 -0300

    Criação do projeto django, instalação e configuração do Docker e Posgres,Criação de um modelo de usuário personalizado

---
+++ users/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth import forms
+
+from .models import User
+
+
+class UserChangeForm(forms.UserChangeForm):
+    class Meta(forms.UserChangeForm.Meta):
+        model = User
+
+class UserCreationForm(forms.UserCreationForm):
+    class Meta(forms.UserCreationForm.Meta):
+        model = User
+
+
+
+
