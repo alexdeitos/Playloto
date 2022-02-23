b1d17bb75ccdce304e5f91859a129e8a3a089fdb
alexdeitos <alexdeitos01@gmail.com>
2022-02-23 09:19:23 -0300

    templates/registration/

--- apploto/urls.py
+++ apploto/urls.py
@@ -1,9 +1,15 @@
 from unicodedata import name
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = "apploto"
 
 urlpatterns = [
     path("",views.Index,name="index")
+]
+
+#Add Django site authentication urls (for login, logout, password management)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
